b1f5ca0b0fe9514ca5e3f6afbba4241bb13bea09
Michael Schlatter Tao <143512472+SchMic2333834@users.noreply.github.com>
2024-11-13 09:55:13 -0500

    Update donnees.php

--- project/ci-news/app/Views/pages/donnees.php
+++ project/ci-news/app/Views/pages/donnees.php
@@ -28,12 +28,27 @@ $db = db_connect();
 
 // Use Query Builder to get the 'temps' column from 'tblDetection' table
 $builder = $db->table('tblActivation');
-$builder->select('temps');
+$builder->select('*');
 $query = $builder->get();
 $data = $query->getResultArray(); // Get all rows as an array of arrays
-
-
-
-
 ?>
+<table>
+    <tr>
+        <th>Activations</th>
+    </tr>
+    <?php
+    foreach ($data as $row) {
+        $OnOff;
+        if ($row['OnOff'] == 0)
+        {
+            $OnOff = 'Désactivé';
+        }
+        else
+        {
+            $OnOff = 'Activé';
+        }
+        echo '<tr><td>'. $row['temps'] .'</td><td>' . $OnOff . '</td></tr>';
+    }
+    ?>
+</table>
 
